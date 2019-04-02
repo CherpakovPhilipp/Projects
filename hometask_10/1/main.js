5d74eb5eac32ff8a91ff8fd1b4c67495a6dd0f25
Cherpakov-Philipp-MSP <cherpakov@msp.ua>
2019-04-02 13:58:32 +0300

    changes in target of addEventListener

--- hometask_10/1/main.js
+++ hometask_10/1/main.js
@@ -1,6 +1,6 @@
-window.addEventListener('load', (event) => {
-  const img = document.querySelector('img');
+const img = document.querySelector('img');
 
+img.addEventListener('load', (event) => {
   console.log('Width: ', img.width);
   console.log('Height: ', img.height);
 });
